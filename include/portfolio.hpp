e52910e06469f76be399de41779cd4cf120498d1
Evan Kirkiles <evkirkiles@gmail.com>
2018-10-08 17:51:25 -0400

    Beginning work on portfolio.

--- include/portfolio.hpp
+++ include/portfolio.hpp
@@ -4,5 +4,44 @@
 
 #ifndef BACKTESTER_PORTFOLIO_HPP
 #define BACKTESTER_PORTFOLIO_HPP
+// Bloomberg includes
+#include "bloombergincludes.hpp"
+// Custom class includes
+#include "events.hpp"
+
+// Class for the Portfolio object which keeps track of holdings and positions for the strategy. This will
+// receive market events and fill events passed in to it by the Strategy event loop, which will be used to recalculate
+// the value of the portfolio. Performance statistic calculations also take place in this class.
+class Portfolio {
+public:
+    // Initializes the portfolio given the symbols and initial capital
+    Portfolio(std::vector<std::string> symbol_list, unsigned int initial_capital, const BloombergLP::blpapi::Datetime& start);
+
+    // Resets the portfolio with a new initial capital amount and a new start date.
+    // This function is used in the constructor as well to initialize the portfolio.
+    void reset_portfolio(unsigned int initial_capital, const BloombergLP::blpapi::Datetime& start);
+
+    // Takes a market event and uses it to update the holdings for a specific stock. This will be called for every
+    // stock in the portfolio (hopefully). I still do not know what happens when there does not exist data for a stock
+    // on a specific day, so hopefully Bloomberg has done all the data cleaning beforehand.
+    void update_market(const events::MarketEvent& event);
+
+    // Takes a fill event and uses it to update the positions and holdings for a specific stock. The fill event comes
+    // from the execution handler and contains a buy or sell quantity that has already been calculated and optimized.
+    void update_fill(const events::FillEvent& event);
+
+private:
+    // Portfolio instance member functions
+    std::vector<std::string> symbol_list;
+    unsigned int initial_capital;
+    BloombergLP::blpapi::Datetime start_date;
+
+    // The maps of the positions at their respective Bloomberg datetimes (quantities of each stock)
+    std::map<BloombergLP::blpapi::Datetime, std::unordered_map<std::string, int>> all_positions;
+    std::unordered_map<std::string, int> current_positions;
+    // The maps of the holdings at their respective Bloomberg datetimes (quantity * price of each stock)
+    std::map<BloombergLP::blpapi::Datetime, std::unordered_map<std::string, double>> all_holdings;
+    std::unordered_map<std::string, double> current_holdings;
+};
 
 #endif //BACKTESTER_PORTFOLIO_HPP
--- src/infrastructure/portfolio.cpp
+++ src/infrastructure/portfolio.cpp
@@ -1,4 +1,41 @@
+#include <utility>
+
 //
 // Created by Evan Kirkiles on 10/8/2018.
 //
 
+// Include corresponding header
+#include "portfolio.hpp"
+
+// Portfolio constructor which simply calls the reset_portfolio function to build the empty
+// holdings and positions maps.
+Portfolio::Portfolio(std::vector<std::string> p_symbol_list, unsigned int p_initial_capital,
+                     const BloombergLP::blpapi::Datetime &p_start) :
+                 symbol_list(std::move(p_symbol_list)) {
+    reset_portfolio(p_initial_capital, p_start);
+}
+
+// Resets (or initializes) the positions and holdings maps. All holdings and all positions will be emptied,
+// and current holdings and current positions will only contain the initial capital amount.
+void Portfolio::reset_portfolio(unsigned int initial_capital, const BloombergLP::blpapi::Datetime &start) {
+
+    // Clear all the maps before writing into them
+    all_positions.clear();
+    all_holdings.clear();
+    current_positions.clear();
+    current_holdings.clear();
+
+    // Build the empty current map from the symbol list
+    for (const auto& symbol : symbol_list) {
+        current_positions[symbol] = 0;
+        current_holdings[symbol] = 0;
+    }
+
+    // Add the non-symbol related rows into the current holdings as well
+    current_holdings["heldcash"] = initial_capital;
+    current_holdings["commission"] = 0;
+    current_holdings["slippage"] = 0;
+    current_holdings["totalholdings"] = initial_capital;
+}
+
+//
