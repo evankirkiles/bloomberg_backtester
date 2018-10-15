d928e2417b0adb9871a9e695bce69460d67bba1d
Evan Kirkiles <evkirkiles@gmail.com>
2018-10-14 23:41:04 -0400

    Built benchmark strategy.

---
+++ include/benchmark.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Evan Kirkiles on 10/14/18.
+//
+
+#ifndef BACKTESTER_BENCHMARK_HPP
+#define BACKTESTER_BENCHMARK_HPP
+// Bloomberg includes
+#include "bloombergincludes.hpp"
+// Custom class includes
+#include "strategy.hpp"
+
+// Contains the logic for a simple Benchmark strategy, in which 100% of the strategy will be invested
+// into SPY upon initialization and left to grow over the period of the backtest.
+class Benchmark : public Strategy {
+public:
+    // Constructor initializes the Strategy parent and buys the SPY shares with the same amount of capital
+    Benchmark(const BloombergLP::blpapi::Datetiem& start, const BloombergLP::blpapi::Datetime& end, unsigned int capital);
+};
+
+#endif //BACKTESTER_BENCHMARK_HPP
---
+++ src/strategy/benchmark.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Evan Kirkiles on 10/14/18.
+//
+
+// Include corresponding header
+#include "benchmark.hpp"
+
+// Initialize the benchmark strategy which simply buys 100% into SPY
+Benchmark::Benchmark(const BloombergLP::blpapi::Datetiem &start, const BloombergLP::blpapi::Datetime &end,
+                     unsigned int capital) :
+        Strategy({"SPY US EQUITY"}, capital, start, end) {
+
+    // Order the S&P
+    order_target_percent("SPY US EQUITY", 1);
+}
