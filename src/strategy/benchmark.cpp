e62606a94907631bce9a22d196342eb834880c1d
Evan Kirkiles <evkirkiles@gmail.com>
2018-10-15 08:19:44 -0400

    Added mac bloomberg library.

--- src/strategy/benchmark.cpp
+++ src/strategy/benchmark.cpp
@@ -6,7 +6,7 @@
 #include "benchmark.hpp"
 
 // Initialize the benchmark strategy which simply buys 100% into SPY
-Benchmark::Benchmark(const BloombergLP::blpapi::Datetiem &start, const BloombergLP::blpapi::Datetime &end,
+Benchmark::Benchmark(const BloombergLP::blpapi::Datetime &start, const BloombergLP::blpapi::Datetime &end,
                      unsigned int capital) :
         Strategy({"SPY US EQUITY"}, capital, start, end) {
 
