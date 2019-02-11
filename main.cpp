9aeee5997744af59d6bc5df887460516c26a44b4
evankirkiles <kirkilese20@kent-school.edu>
2019-02-11 00:04:35 -0500

    Fixed datetime bug.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 int main(int argc, char* argv[]) {
 
     // Run a Basic Algo
-    ALGO_Momentum1 alg(date_funcs::get_now(), BloombergLP::blpapi::Datetime(2019, 2, 31, 0, 0, 0), 1000000);
+    ALGO_Momentum1 alg(BloombergLP::blpapi::Datetime(2014, 1, 31, 0, 0, 0), BloombergLP::blpapi::Datetime(2016, 1, 31, 0, 0, 0), 1000000);
 //    alg.message("Beginning live paper trading of momentum algorithm...");
     // Run the algorithm
     alg.run();
